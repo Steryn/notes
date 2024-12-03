f06bd9d434db0388706fb50093c64df10b7f987c
JaylenLiu <jaylen.liu@biposervice.com>
2024-12-03 23:40:03 +0800

    build: init set up

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
-  // base: "/blog-code/",
+  base: "/blog-code/",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
