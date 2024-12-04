fd1f830797d77992e3e8b9fa24631ed99329e663
JaylenLiu <jaylen.liu@biposervice.com>
2024-12-04 09:27:31 +0800

    chore: change pkg name

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,30 +2,34 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
+  title: "Coding Notes",
   description: "A VitePress Site",
-  base: "/blog-code/",
+  base: "/notes/",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
+      { text: "主页", link: "/" },
+      { text: "Vue", link: "/vue/README" },
+      // { text: "Angular", link: "/angular" },
+      // { text: "React", link: "/react" },
+      // {
+      //   text: "Others",
+      //   items: [{ text: "Example", link: "/markdown-examples" }],
+      // },
     ],
 
     sidebar: [
       {
-        text: "Examples",
+        text: "Vue",
         items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
+          { text: "Readme", link: "/vue/README" },
+          { text: "Vue3", link: "/vue/vue3" },
         ],
       },
     ],
 
-    socialLinks: [
-      { icon: "github", link: "https://github.com/Steryn/blog-code" },
-    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/Steryn/notes" }],
     docFooter: {
       prev: "上一页",
       next: "下一页",
@@ -42,7 +46,7 @@ export default defineConfig({
       },
     },
     editLink: {
-      pattern: `https://github.com/Steryn/blog-code/tree/main/docs/:path`,
+      pattern: `https://github.com/Steryn/notes/tree/main/docs/:path`,
       text: "去编辑此文档",
     },
     search: {
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,37 @@
 layout: home
 
 hero:
-  name: "My Awesome Blog"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "Awesome Coding Notes"
+  text: "做个记录，做个总结，做个分享"
+  tagline: 一句代码，一个Bug, 一个Bug，一个Bug...
   actions:
     - theme: brand
-      text: Markdown Examples
+      text: 开始 →
       link: /markdown-examples
     - theme: alt
-      text: API Examples
+      text: 文章
       link: /api-examples
-      
+
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 做个记录
+    details: 记录整理的代码，方便以后查阅
+  - title: 做个总结
+    details: 总结写过的代码，防止以后忘记
+  - title: 做个分享
+    details: 分享遇到的代码，方便以后查阅
+  - title: 做个分类
+    details: 将代码分类，整理好思维
+  - title: Angular
+    details: 记录Angular的知识点
+  - title: Vue
+    details: 记录Vue3, Vue2, VueRouter等
+  - title: React
+    details: 记录React的知识点
+  - title: Node
+    details: 记录Node的知识点
+  # - title: TypeScript
+  #   details: 记录TypeScript的知识点
+  # - title: JavaScript
+  #   details: 记录JavaScript的知识点
 ---
 
